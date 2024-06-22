25f4f10b7f28d971c9728ed01ab051594b4afaec
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2024-06-22 18:14:42 +0530

    Movie details page

--- js/script.js
+++ js/script.js
@@ -12,8 +12,7 @@ async function displayPopularMovies() {
         div.classList.add('card');
         div.innerHTML = `
           <a href="movie-details.html?id=${movie.id}">
-            ${
-                movie.poster_path
+            ${movie.poster_path
                 ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
                 : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
             }
@@ -25,7 +24,7 @@ async function displayPopularMovies() {
             </p>
           </div>  
         `;
-            document.querySelector('#popular-movies').appendChild(div);
+        document.querySelector('#popular-movies').appendChild(div);
     });
 }
 
@@ -39,8 +38,7 @@ async function displayPopularShows() {
         div.classList.add('card');
         div.innerHTML = `
           <a href="tv-details.html?id=${show.id}">
-            ${
-                show.poster_path
+            ${show.poster_path
                 ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />`
                 : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
             }
@@ -52,8 +50,53 @@ async function displayPopularShows() {
             </p>
           </div>  
         `;
-            document.querySelector('#popular-shows').appendChild(div);
+        document.querySelector('#popular-shows').appendChild(div);
     });
+}
+
+// Display movie details
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1];
+    const movie = await fetchAPIData(`movie/${movieId}`);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+        <div>
+        ${movie.poster_path
+            ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
+            : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+        }
+        </div>
+        <div>
+          <h2>${movie.title}</h2>
+          <p>
+            <i class="fas fa-star text-primary"></i>
+            ${movie.vote_average.toFixed(1)} / 10
+          </p>
+          <p class="text-muted">Release Date: ${movie.release_date}</p>
+          <p>${movie.overview}</p>
+          <h5>Genres</h5>
+          <ul class="list-group">
+            ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+          </ul>
+          <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+        </div>
+      </div>
+      <div class="details-bottom">
+        <h2>Movie Info</h2>
+        <ul>
+          <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+          <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+          <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+          <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`).join('')}</div>
+      </div>
+    `;
+
+    document.querySelector('#movie-details').appendChild(div);
 }
 
 // Fetch data from the API
@@ -89,6 +132,10 @@ function highlightActiveLink() {
     });
 }
 
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Initial App
 function initial() {
     switch (global.currentPage) {
@@ -102,7 +149,7 @@ function initial() {
             break;
 
         case '/movie-details.html':
-            console.log('Movie Details');
+            displayMovieDetails();
             break;
 
         case '/tv-details.html':
