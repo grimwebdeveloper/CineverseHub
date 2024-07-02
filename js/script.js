9e8b8ded539740c90a80445c917095bd5b22b05c
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2024-07-02 22:23:12 +0530

    Display popular movies created

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
-const links = document.querySelectorAll('.nav-link');
 const global = {
     currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+    const { results } = await fetchAPIData('movie/popular');
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href='movie-details.html?id=${movie.id}'>
+            ${movie.poster_path
+                ? `<img src='https://image.tmdb.org/t/p/w500${movie.poster_path}' class='card-img-top' alt='${movie.title}'/>`
+                : `<img src='images/no-image.jpg' class='card-img-top' alt='${movie.title}'/>`
+            }
+          </a>
+          <div class='card-body'>
+            <h5 class='card-title'>${movie.title}</h5>
+            <p class='card-text'>
+              <small class='text-muted'>Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+async function fetchAPIData(endpoint) {
+    const API_KEY = '8e910b8001b97796872ce25e3010e43b';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US&page=1`);
+    const data = await response.json();
+    return data;
+}
+
 function highlightActiveLink() {
+    const links = document.querySelectorAll('.nav-link');
     links.forEach(link => {
         if (link.getAttribute('href') === global.currentPage) {
             link.classList.add('active');
@@ -13,9 +45,9 @@ function highlightActiveLink() {
 
 function init() {
     switch (global.currentPage) {
-        case '/':   
+        case '/':
         case '/index.html':
-            console.log('Home', `and the path is '${global.currentPage}'`);
+            displayPopularMovies();
             break;
 
         case '/shows.html':
