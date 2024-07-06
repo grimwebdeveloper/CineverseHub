8cf2d5159a6bc628d03c304d50495ffc983179b5
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2024-07-06 21:05:40 +0530

    Search function and custom alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1
+  },
+  api: {
+    apiKey: '8e910b8001b97796872ce25e3010e43b',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  }
 };
 
 async function displayPopularMovies() {
@@ -179,6 +189,19 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
 async function displaySlider() {
   const { results } = await fetchAPIData('movie/now_playing');
 
@@ -226,12 +249,26 @@ function initSwiper() {
 }
 
 async function fetchAPIData(endpoint) {
-  const API_KEY = '8e910b8001b97796872ce25e3010e43b';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US&page=1`);
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
   const data = await response.json();
 
   hideSpinner();
@@ -254,6 +291,13 @@ function highlightActiveLink() {
       link.classList.add('active');
     }
   });
+}
+
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
 }
 
 function addCommasToNumber(number) {
@@ -281,7 +325,7 @@ function init() {
       break;
 
     case '/search.html':
-      console.log('Search', `and the path is '${global.currentPage}'`);
+      search();
       break;
 
     default:
