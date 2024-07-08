7bd7fb00dc6bc399c30f3b9429c3a3bc3c861347
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2024-07-08 20:42:58 +0530

    Pagination added, Project finished and Improvement required

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
     term: '',
     type: '',
     page: 1,
-    totalPages: 1
+    totalPages: 1,
+    totalResults: 0
   },
   api: {
     apiKey: '8e910b8001b97796872ce25e3010e43b',
@@ -195,7 +196,10 @@ async function search() {
   global.search.type = urlParams.get('type');
   global.search.term = urlParams.get('search-term');
   if (global.search.term !== '' && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
     if (results.length === 0) {
       showAlert();
       return;
@@ -208,6 +212,9 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
   results.forEach(result => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -239,8 +246,47 @@ function displaySearchResults(results) {
             </p>
           </div>
         `;
+
+    document.querySelector('#search-results-heading').innerHTML = `
+          <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+        `;
+
     document.querySelector('#search-results').appendChild(div);
   });
+
+  displayPagination();
+}
+
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+  document.querySelector('#pagination').appendChild(div);
+
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const {results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const {results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  });
+
 }
 
 async function displaySlider() {
@@ -309,7 +355,7 @@ async function searchAPIData() {
 
   showSpinner();
 
-  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
   const data = await response.json();
 
   hideSpinner();
